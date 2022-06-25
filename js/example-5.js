74bdd8e9bbd1368f1286691ba12ae442898b1343
AndrewMLion <andrewmlion@gmail.com>
2022-06-25 10:28:28 +0300

    решение 5-й задачи

--- js/example-5.js
+++ js/example-5.js
@@ -7,4 +7,12 @@
 Якщо ні, додай у кінець значення link цей символ, але тільки в тому випадку, якщо в link є підрядок "my-site".
 */
 
-let link = "https://my-site.com/about";
+let link = 'https://my-site.com/about';
+
+const end = '/';
+
+if (link.includes('my-site')) {
+    console.log(link + end);
+} else {
+    console.log(link);
+}
--- js/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const answ = prompt("Яка офіційна назва в JavaScript?");
-console.log(answ);
-
-answ.toLocaleLowerCase() === "ECMAScript".toLocaleLowerCase()
-	? alert("Правільно!")
-	: alert("Це ECMAScript!");
